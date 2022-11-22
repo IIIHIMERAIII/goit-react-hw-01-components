39569ac12149180d5a3d4e0913dcd3e0a014959d
Maksym <PTNPNH722@gmail.com>
2022-11-22 19:46:19 +0200

    fix 1

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -18,22 +18,6 @@ export const Statistics = ({title, stats}) => {
                     <span class="percentage">{item.percentage}%</span>
                 </li>)
                 })}
-                {/* <li style={{ backgroundColor : getRandomHexColor() }} class="item">
-                    <span class="label">{props.stats[0].label}</span>
-                    <span class="percentage">{props.stats[0].percentage}%</span>
-                </li>
-                <li style={{ backgroundColor : getRandomHexColor() }}  class="item">
-                    <span class="label">{props.stats[1].label}</span>
-                    <span class="percentage">{props.stats[1].percentage}%</span>
-                </li>
-                <li style={{ backgroundColor : getRandomHexColor() }} class="item">
-                    <span class="label">{props.stats[2].label}</span>
-                    <span class="percentage">{props.stats[2].percentage}%</span>
-                </li>
-                <li style={{ backgroundColor : getRandomHexColor() }} class="item">
-                    <span class="label">{props.stats[3].label}</span>
-                    <span class="percentage">{props.stats[3].percentage}%</span>
-                </li> */}
             </ul>
         </section>
     );
